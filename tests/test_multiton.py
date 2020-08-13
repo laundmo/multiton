59b979a4fe7218e4b64703176b90eb4d1ad98773
laundmo <laurinschmidt2001@gmail.com>
2020-08-14 01:41:46 +0200

    test: fixed test to work with the recent refactor

--- tests/test_multiton.py
+++ tests/test_multiton.py
@@ -10,7 +10,9 @@ from multiton import MultitonMetaFactory
 @pytest.fixture
 def make_multiton():
     def _make_multiton(*args, **kwargs):
-        class TestMultiton(metaclass=MultitonMetaFactory(*args, **kwargs)):
+        print(args, kwargs)
+        meta = MultitonMetaFactory(*args, **kwargs)
+        class TestMultiton(metaclass=meta):
             def __init__(self, a, b, kw_a=None, kw_b=None) -> None:
                 self.a = a
                 self.b = b
@@ -22,10 +24,29 @@ def make_multiton():
 
 
 def test_positional_no_keyword(make_multiton):
-    MyMultiton = make_multiton([0, 1])
+    MyMultiton = make_multiton(0, 1)
     a = MyMultiton(5, 6)
     b = MyMultiton(5, 6)
     assert a is b
     a = MyMultiton(5, 6)
     b = MyMultiton(5, 7)
     assert not (a is b)
+
+def test_keyword_no_positional(make_multiton):
+    MyMultiton = make_multiton(kw_a=None)
+    a = MyMultiton(None, None, kw_a=8, kw_b="test")
+    b = MyMultiton(None, None, kw_a=8, kw_b="test")
+    assert a is b
+    a = MyMultiton(None, None, kw_a=8, kw_b="test")
+    b = MyMultiton(None, None, kw_a=5, kw_b="test")
+    assert not (a is b)
+
+def test_both_getter(make_multiton):
+    MyMultiton = make_multiton((0, lambda x: x["a"]), 1, kw_a=lambda x: x[0], kw_b=None)
+    a = MyMultiton({"a":1}, 5, kw_a=[8, 9], kw_b="test")
+    b = MyMultiton({"a":1}, 5, kw_a=[8, 10], kw_b="test")
+    assert a is b
+    a = MyMultiton({"a":1}, 5, kw_a=[8, 9], kw_b="test")
+    b = MyMultiton({"a":2}, 5, kw_a=[8, 9], kw_b="test")
+    c = MyMultiton({"a":1}, 5, kw_a=[5, 9], kw_b="test")
+    assert not (a is b) and not (a is c)
